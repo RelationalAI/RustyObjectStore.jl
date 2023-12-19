80a2227801ee563302c7515d3f64b46216da49b1
Nick Robinson <npr251@gmail.com>
2023-12-19 17:38:17 +0000

    Mark some tests fixed

--- test/azure_blobs_exception_tests.jl
+++ test/azure_blobs_exception_tests.jl
@@ -163,7 +163,7 @@ end # @testitem
 
     function test_status(method, response_status, headers=nothing)
         @assert method === :GET || method === :PUT
-        nretries = Ref(0)
+        nrequests = Ref(0)
         response_body = "response body from the dummy server"
         account = "myaccount"
         container = "mycontainer"
@@ -175,7 +175,7 @@ end # @testitem
                 # This is the exploratory ping from connect_and_test in lib.rs
                 return HTTP.Response(404, "Yup, still doesn't exist")
             end
-            nretries[] += 1
+            nrequests[] += 1
             response = isnothing(headers) ? HTTP.Response(response_status, response_body) : HTTP.Response(response_status, headers, response_body)
             return response
         end
@@ -195,7 +195,7 @@ end # @testitem
             close(http_server)
         end
         wait(http_server)
-        return nretries[]
+        return nrequests[]
     end
 
     # See https://learn.microsoft.com/en-us/rest/api/searchservice/http-status-codes
@@ -203,54 +203,54 @@ end # @testitem
     @testset "400: Bad Request" begin
         # Returned when there's an error in the request URI, headers, or body. The response body
         # contains an error message explaining what the specific problem is.
-        nretries = test_status(:GET, 400)
-        @test nretries == 1 broken=true
-        nretries = test_status(:PUT, 400)
-        @test nretries == 1 broken=true
+        nrequests = test_status(:GET, 400)
+        @test nrequests == 1
+        nrequests = test_status(:PUT, 400)
+        @test nrequests == 1
     end
 
     @testset "403: Forbidden" begin
         # Returned when you pass an invalid api-key.
-        nretries = test_status(:GET, 403)
-        @test nretries == 1 broken=true
-        nretries = test_status(:PUT, 403)
-        @test nretries == 1 broken=true
+        nrequests = test_status(:GET, 403)
+        @test nrequests == 1
+        nrequests = test_status(:PUT, 403)
+        @test nrequests == 1
     end
 
     @testset "404: Not Found" begin
-        nretries = test_status(:GET, 404)
-        @test nretries == 1
+        nrequests = test_status(:GET, 404)
+        @test nrequests == 1
     end
 
     @testset "405: Method Not Supported" begin
-        nretries = test_status(:GET, 405, ["Allow" => "PUT"])
-        @test nretries == 1 broken=true
-        nretries = test_status(:PUT, 405, ["Allow" => "GET"])
-        @test nretries == 1 broken=true
+        nrequests = test_status(:GET, 405, ["Allow" => "PUT"])
+        @test nrequests == 1
+        nrequests = test_status(:PUT, 405, ["Allow" => "GET"])
+        @test nrequests == 1
     end
 
     @testset "409: Conflict" begin
         # Returned when write operations conflict.
         # NOTE: We currently don't retry but maybe we should? This is probably a case where the
         # retry logic should add more noise to the backoff so that multiple writers don't collide on retry.
-        nretries = test_status(:GET, 409)
-        @test nretries == max_retries broken=true
-        nretries = test_status(:PUT, 409)
-        @test nretries == max_retries broken=true
+        nrequests = test_status(:GET, 409)
+        @test nrequests == 1 + max_retries broken=true
+        nrequests = test_status(:PUT, 409)
+        @test nrequests == 1 + max_retries broken=true
     end
 
     @testset "412: Precondition Failed" begin
         # Returned when an If-Match or If-None-Match header's condition evaluates to false
-        nretries = test_status(:GET, 412)
-        @test nretries == 1
-        nretries = test_status(:PUT, 412)
-        @test nretries == 1
+        nrequests = test_status(:GET, 412)
+        @test nrequests == 1
+        nrequests = test_status(:PUT, 412)
+        @test nrequests == 1
     end
 
     @testset "413: Content Too Large" begin
         # https://learn.microsoft.com/en-us/rest/api/storageservices/put-blob?tabs=shared-access-signatures#remarks
-        nretries = test_status(:PUT, 413)
-        @test nretries == 1 broken=true
+        nrequests = test_status(:PUT, 413)
+        @test nrequests == 1
     end
 
     @testset "429: Too Many Requests" begin
@@ -258,34 +258,34 @@ end # @testitem
         # (and we don't know if Azure actually sets it)
         # NOTE: This can happen when Azure is throttling us, so it might be a good idea to retry with some
         # larger initial backoff (very eager retries probably only make the situation worse).
-        nretries = test_status(:GET, 429, ["Retry-After" => 10])
-        @test nretries == max_retries broken=true
-        nretries = test_status(:PUT, 429, ["Retry-After" => 10])
-        @test nretries == max_retries broken=true
+        nrequests = test_status(:GET, 429, ["Retry-After" => 10])
+        @test nrequests == 1 + max_retries broken=true
+        nrequests = test_status(:PUT, 429, ["Retry-After" => 10])
+        @test nrequests == 1 + max_retries broken=true
     end
 
     @testset "502: Bad Gateway" begin
         # This error occurs when you enter HTTP instead of HTTPS in the connection.
-        nretries = test_status(:GET, 502)
-        @test nretries == 1 broken=true
-        nretries = test_status(:PUT, 502)
-        @test nretries == 1 broken=true
+        nrequests = test_status(:GET, 502)
+        @test nrequests == 1 broken=true
+        nrequests = test_status(:PUT, 502)
+        @test nrequests == 1 broken=true
     end
 
     @testset "503: Service Unavailable" begin
         # NOTE: This seems similar to 429 and the Azure docs specifically say:
         #    Important: In this case, we highly recommend that your client code back off and wait before retrying
-        nretries = test_status(:GET, 503)
-        @test nretries == max_retries broken=true
-        nretries = test_status(:PUT, 503)
-        @test nretries == max_retries broken=true
+        nrequests = test_status(:GET, 503)
+        @test nrequests == 1 + max_retries
+        nrequests = test_status(:PUT, 503)
+        @test nrequests == 1 + max_retries
     end
 
     @testset "504: Gateway Timeout" begin
         # Azure AI Search listens on HTTPS port 443. If your search service URL contains HTTP instead of HTTPS, a 504 status code is returned.
-        nretries = test_status(:GET, 504)
-        @test nretries == 1 broken=true
-        nretries = test_status(:PUT, 504)
-        @test nretries == 1 broken=true
+        nrequests = test_status(:GET, 504)
+        @test nrequests == 1 broken=true
+        nrequests = test_status(:PUT, 504)
+        @test nrequests == 1 broken=true
     end
 end
