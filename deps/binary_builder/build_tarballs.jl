bc801ba1047fef5c715ee8c6e64c60a9ad474250
Nick Robinson <npr251@gmail.com>
2023-12-21 01:35:38 +0000

    fixup! Add BinaryBuilder build_tarballs.jl script

--- deps/binary_builder/build_tarballs.jl
+++ deps/binary_builder/build_tarballs.jl
@@ -7,13 +7,7 @@ name = "object_store_ffi"
 version = v"0.1.0"
 
 sources = [
-    # DirectorySource(joinpath(deps_dir, "object_store_ffi")),
-    # # OR something like this (untested):
-    # # https://github.com/RelationalAI/ObjectStore.jl/commit/a5b053f824d8e165211a67c4a84adc520fbac9ae
-    GitSource(
-        "https://github.com/RelationalAI/ObjectStore.jl/deps/object_store_ffi",
-        "b4150213e6b9ce701e05abbd511c92813d90ed62"
-    ),
+    DirectorySource(joinpath(deps_dir, "object_store_ffi")),
 ]
 
 # Bash recipe for building across all platforms
@@ -23,14 +17,14 @@ cargo rustc --release --lib --crate-type=cdylib
 install -Dvm 755 "target/${rust_target}/release/libobject_store_ffi.${dlext}" "${libdir}/libobject_store_ffi.${dlext}"
 """
 
-# platforms = supported_platforms()
-# # Our Rust toolchain for i686 Windows is unusable
-# filter!(p -> !Sys.iswindows(p) || arch(p) != "i686", platforms)
-# # Also, can't build cdylib for Musl systems
-# filter!(p -> libc(p) != "musl", platforms)
-
-# # For local Mac testing:
-platforms = [Platform("aarch64", "macos")]
+# We could potentially support more platforms, if required.
+# Except perhaps i686 Windows and Musl systems.
+platforms = [
+    Platform("aarch64", "macos"),
+    Platform("x86_64",  "macos"),
+    Platform("aarch64", "linux"),
+    Platform("x86_64",  "linux"),
+]
 
 # The products that we will ensure are always built
 products = [
