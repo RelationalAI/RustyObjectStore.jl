002c8c32c805273aef0e36a991feb4f58c65770a
Nick Robinson <npr251@gmail.com>
2024-01-08 13:26:38 +0000

    Try support local development setup

---
+++ test/release_test.jl
@@ -0,0 +1,7 @@
+# We should always be using the JLL package.
+# We allow seeting the environment variable OBJECT_STORE_LIB to override this
+# for development reasons, but it should never be set in CI.
+@testitem "Using object_store_ffi_jll" begin
+    using object_store_ffi_jll
+    @test ObjectStore.rust_lib == object_store_ffi_jll.libobject_store_ffi
+end
