fbaaebdbd6922560e6d1514263a21e3df4e649d4
Nick Robinson <npr251@gmail.com>
2024-01-11 00:18:21 +0000

    Add README and docstrings for Julia API (#17)
    * Add docstrings to Julia API
    
    * WIP README
    
    * WIP README design notes
    
    * Update test to more specific exception types
    
    * More readme
    
    * fixup! More readme
    
    * Update README.md

---
+++ test/azure_api_tests.jl
@@ -0,0 +1,8 @@
+@testitem "AzureCredentials" begin
+    # access key is obscured when printing
+    @test repr(AzureCredentials("a", "b", "c", "d")) == "AzureCredentials(\"a\", \"b\", \"*****\", \"d\")"
+    # host is optional
+    @test AzureCredentials("a", "b", "c") == AzureCredentials("a", "b", "c", "")
+    # host is not shown if not set
+    @test repr(AzureCredentials("a", "b", "c")) == "AzureCredentials(\"a\", \"b\", \"*****\")"
+end
