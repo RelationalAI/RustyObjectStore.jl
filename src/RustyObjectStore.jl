ed3cf60ad17eb53625c4617d109c1eaa5fbfba18
Justin Funston <justin.funston@relational.ai>
2024-01-22 09:09:58 -0800

    Address PR comments

--- src/RustyObjectStore.jl
+++ src/RustyObjectStore.jl
@@ -70,7 +70,7 @@ const DEFAULT_CONFIG = StaticConfig(
     cache_capacity=20,
     cache_ttl_secs=30 * 60,
     cache_tti_secs=5 * 60,
-    multipart_put_threshold=8 * 1024 * 1024,
+    multipart_put_threshold=10 * 1024 * 1024,
     multipart_get_threshold=8 * 1024 * 1024,
     multipart_get_part_size=8 * 1024 * 1024
 )
